67945db71ed2c62114046e8bb38d316cad12351b
Akash242099 <52275484+Akash242099@users.noreply.github.com>
2024-07-29 12:08:47 +0530

    Add files via upload

---
+++ Model/Jobs.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Model
+{
+    [Serializable]
+    public class Jobs
+    {
+        
+
+        public int Id { get; set; }
+        public DateTime StartTimestamp { get; set; }
+        public DateTime EndTimestamp { get; set; }
+        public string SourceIp { get; set; }
+        public string DestinationIp { get; set; }
+        public string SourcePath { get; set; }
+        public string DestPath { get; set; }
+        public int CreatedBy { get; set; }
+        public string Status { get; set; }
+        public string Comment { get; set; }
+    }
+}
---
+++ Model/Users.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Model
+{
+    public class Users
+    {
+        
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string EmailId { get; set; }
+        public int ContantNo { get; set; }
+        public bool isAdmin { get; set; }
+        [Required(ErrorMessage = "Password is Required")]
+        public string PassWord { get; set; }
+
+    }
+}
